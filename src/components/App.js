2a8b717ce207bfab44e5501e20130e1b89e81f75
Ashish Raina <ashish.raina@bookmyshow.com>
2020-08-21 14:11:03 +0530

    started a project

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class App extends Component {
+    render() {
+        return (
+            <div>
+                My React App!!
+            </div>
+        )
+    }
+}
+
+export default App
